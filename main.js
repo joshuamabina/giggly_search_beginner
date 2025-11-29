438d492e85810057120caea2d17d12529ab35a84
Joshua Mabina <mabinajoshua@gmail.com>
2025-11-29 01:51:00 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// console.log("Hello world!");
+// alert("Hello world!");
+
+let searchButton = document.getElementById("searchButton");
+searchButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    
+    let searchInput = document.getElementById("searchInput");
+    let searchInputValue = searchInput.value.trim();
+
+    if (searchInputValue == "" || searchInputValue == null) return false;
+
+    alert(`You have searched for: ${searchInput.value}`);
+});
