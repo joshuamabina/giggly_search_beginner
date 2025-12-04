14a122a8da6938a4b13ad80c4a0049fa78b71ef6
Joshua Mabina <mabinajoshua@gmail.com>
2025-12-04 23:49:37 +0300

    wip

--- main.js
+++ main.js
@@ -9,15 +9,18 @@ searchButton.addEventListener("click", function (e) {
         alert("Type something fun to search for, first");
 
         return false;
-    } 
-        
+    }
+
     alert(`You have searched for: ${searchInput.value}`);
+
+    const form = this.closest("form");
+    form.submit();
 });
 
 let clearButton = document.getElementById("clearButton");
 clearButton.addEventListener("click", function (e) {
     e.preventDefault();
-    
+
     searchInput.value = "";
 });
 
